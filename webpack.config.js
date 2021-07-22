f2f364f9e340e12ecfe62b59edb60836204a433f
Ivan Sollima <i.sollima@accenture.com>
2021-07-22 16:23:33 +0200

    Fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/ts/main.ts',
+    entry: './src/ts/fetch.ts',
     mode: 'development',
     module: {
         rules: [
